1d2b57cbe30af7337c7b7a13c414da0bf6b355e7
Antoine Guérette <antoine.guerette@gmail.com>
2021-11-14 17:39:59 -0500

    Séquence de début

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour
+{
+    public RawImage trois;
+    public RawImage deux;
+    public RawImage un;
+    public RawImage actionBig;
+    public RawImage actionSmall;
+
+    void startGame() {
+        Invoke("hideThree", 1.0f);
+    }
+
+    void hideThree() {
+        trois.enabled = false;
+        Invoke("hideTwo", 1.0f);
+    }
+
+    void hideTwo() {
+        deux.enabled = false;
+        Invoke("hideOne", 1.0f);
+    }
+
+    void hideOne() {
+        un.enabled = false;
+        Invoke("hideActionBig", 1.0f);
+    }
+
+    void hideActionBig() {
+        actionBig.enabled = false;
+        Invoke("hideActionSmall", 0.5f);
+    }
+
+    void hideActionSmall() {
+        actionSmall.enabled = false;
+        Invoke("send", 0.5f);
+    }
+
+    void send() {
+        this.GetComponent<GameManager>().SendMessage("debut");
+    }
+}
