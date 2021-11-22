1d39f2420b997b695d0e22554b2758f0bb061d86
Antoine Guérette <antoine.guerette@gmail.com>
2021-11-22 16:43:18 -0500

    Laitue tomate scene modelisateurs

--- Assets/Scripts/Personnage/Objets.cs
+++ Assets/Scripts/Personnage/Objets.cs
@@ -17,6 +17,7 @@ public class Objets : MonoBehaviour
     void Update() {
         if (isCarrying) {
             ingredient.transform.position = main.transform.position + offset;
+            Debug.Log(ingredient.tag); 
         }
     }
 
